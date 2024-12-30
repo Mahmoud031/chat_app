431958be679794edd04c4f21ee3b614eb48f3637
Mahmoud031 <mahmoudrady007@gmail.com>
2024-12-30 15:23:51 +0200

    use cubit for login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/screens/chat_page.dart';
+import 'package:chat_app/screens/cubits/login_cubit/login_cubit.dart';
 import 'package:chat_app/screens/login_page.dart';
 import 'package:chat_app/screens/sign_up.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,13 +21,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        routes: {
-          LoginPage.id: (context) => const LoginPage(),
-          SignUp.id: (context) => const SignUp(),
-          ChatPage.id: (context) =>  ChatPage()
-        },
-        initialRoute: LoginPage.id);
+    return BlocProvider(
+      create: (context) => LoginCubit(),
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          routes: {
+            LoginPage.id: (context) =>  LoginPage(),
+            SignUp.id: (context) => const SignUp(),
+            ChatPage.id: (context) => ChatPage()
+          },
+          initialRoute: LoginPage.id),
+    );
   }
 }
